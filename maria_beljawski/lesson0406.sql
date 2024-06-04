c7ad2ad25ae52db3aded064a22f9485fd391e2f2
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-06-04 10:17:57 +0200

    second task

--- maria_beljawski/lesson0406.sql
+++ maria_beljawski/lesson0406.sql
@@ -1,6 +1,15 @@
+-- Task 1:
 SELECT
 customerName,
 Country
 FROM Customers
 where
 country in ("USA", "France")
+
+-- Task2:
+SELECT
+Orders.OrderID,
+Employees.LastName
+FROM Orders
+join Employees on Employees.EmployeeID = Orders.EmployeeID
+Where Employees.LastName like '%f%'
